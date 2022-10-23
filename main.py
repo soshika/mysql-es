11106508f5c08d266982dbd023f9f1e6f62fe45f
Sobhan Shafiei <soshika1010@gmail.com>
2022-10-23 10:07:24 +0330

    create random posts is up & running

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ db = mysql.connector.connect(
   database="blogs"
 )
 
-
 def get_10_admins():
     cursor = db.cursor()
     cursor.execute("SELECT id FROM users WHERE is_admin=1 ORDER BY rand() LIMIT 10;")
@@ -17,15 +16,31 @@ def get_10_admins():
     ids = [user[0] for user in users]
     return ids
 
+def get_5_non_admins():
+    cursor = db.cursor()
+    cursor.execute("SELECT id FROM users WHERE is_admin=0 ORDER BY rand() LIMIT 5;")
+    users = cursor.fetchall()
+    ids = [user[0] for user in users]
+    return ids
+
+
+def create_random_post(sender_id):
+    fake = Faker()
+
+    title = str(fake.text()).split('.')[0]
+    content = fake.text()
+
+    cursor = db.cursor()
+    sql = "INSERT INTO posts (title, content, sender_id) VALUES (%s, %s, %s);"
+    val = (title, content, str(sender_id))
+    cursor.execute(sql, val)
+    db.commit()
+    print(cursor.rowcount, "record inserted.")
 
 if __name__ == "__main__":
 
     admin_ids = get_10_admins()
-    for id in admin_ids:
-        print(id)
+    # non_admin_ids = get_5_non_admins()
 
-    # fake = Faker('fa_IR')
-    # name = fake.name()
-    # address = fake.address()
-    # text = fake.text()
-    # print(name)
+    for admin in admin_ids:
+        create_random_post(admin)

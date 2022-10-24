ffc65c342ea92db210d60baebc755ed047a6241a
Sobhan Shafiei <soshika1010@gmail.com>
2022-10-24 08:36:58 +0330

    get messages fro queue

---
+++ rabbitmq-consume.py
@@ -0,0 +1,23 @@
+import pika
+import time
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='test', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    # time.sleep(body.count(b'.'))
+    time.sleep(3)
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='test', on_message_callback=callback)
+
+channel.start_consuming()

60d4d3af2cdc92d8de64d025178f8e2efa6ae0a3
Sobhan Shafiei <soshika1010@gmail.com>
2022-10-25 15:57:36 +0330

    now send request to server and push the result into elasticsearchg

--- rabbitmq-consume.py
+++ rabbitmq-consume.py
@@ -1,6 +1,8 @@
 import pika
 import time
 import json
+import requests
+
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
@@ -10,11 +12,22 @@ channel.queue_declare(queue='test', durable=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
 
+def req(body):
+    url = "http://fastmovie.online:9091/es"
+    payload = json.dumps(body)
+    headers = {
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     json_body = json.loads(str(body, 'utf-8'))
-    print(json_body)
-    return 
+    req(json_body)
     time.sleep(1)
     print(" [x] Done")
     ch.basic_ack(delivery_tag=method.delivery_tag)
